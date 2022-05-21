0688effe76155bb02ed8002123d3a4dfec88565b
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-21 22:28:11 +0800

    feat: add edit resaurant function

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.get('/restaurants/new', (req, res) => {
   res.render('new')
 })
 
+// creat restaurant
 app.post('/restaurants', (req, res) => {
   const restaurantNew = req.body
   return Restaurant.create({
@@ -67,6 +68,35 @@ app.get('/restaurants/:id', (req, res) => {
   return Restaurant.findById(id)
     .lean()
     .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+// edit restaurant
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const restaurantEdit = req.body
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = restaurantEdit.name
+      restaurant.name_en = restaurantEdit.name_en
+      restaurant.category = restaurantEdit.category
+      restaurant.image = restaurantEdit.image
+      restaurant.location = restaurantEdit.location
+      restaurant.phone = restaurantEdit.phone
+      restaurant.google_map = restaurantEdit.google_map
+      restaurant.rating = restaurantEdit.rating
+      restaurant.description = restaurantEdit.description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
 
