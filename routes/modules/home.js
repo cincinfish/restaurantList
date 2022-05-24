5812b6e6491cefc5c22227481c60316fae01b100
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-24 21:05:10 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,23 +6,34 @@ const Restaurant = require('../../models/restaurant')
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
-    .sort({ _id: 'asc' })
+    .sort({ _id: "asc" })
     .then(restaurantList => res.render('index', { restaurantList }))
     .catch(error => console.error(error))
 })
 
 //search restaurant
 router.get('/search', (req, res) => {
-
-  const keyword = req.query.keyword
-  if (!keyword) {
-    res.render('index', { keyword })
-    return
+  // if (!req.query.keyword) {
+  //   return res.redirect("/")
+  // }
+  let keyword = req.query.keyword
+  const sortValue = req.query.sort
+  const sortOptions = {
+    "id-asc": { id: "asc" },
+    "name-asc": { name: "asc" },
+    "name-desc": { name: "desc" },
+    "rating-asc": { rating: "asc" },
+    "rating-desc": { rating: "desc" },
+  }
+  const sort = sortValue ? { [sortValue]: true } : { "id-asc": true }
+  if (!req.query.keyword) {
+    keyword = ""
   }
   const keywords = [...new Set(keyword.toLowerCase().split(',').map(item => item.trim()))]
 
   Restaurant.find()
     .lean()
+    .sort(sortOptions[sortValue])
     .then(restaurantList => {
       const searchRestaurant = []
       for (word of keywords) {
@@ -35,9 +46,10 @@ router.get('/search', (req, res) => {
           searchRestaurant.push(serResult[i])
         }
       }
-      res.render('index', { restaurantList: searchRestaurant, keyword })
+      res.render('index', { restaurantList: searchRestaurant, keyword, sort })
     })
     .catch(error => console.log(error))
+
 })
 
 module.exports = router
