53c9d5ffd4cd6352578467e555e0c9aeaeb40a28
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-17 17:54:05 +0800

    feat: add express-session & user login feature via passport

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require("express-session")
 const routes = require('./routes')
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 require('dotenv').config()
-
 require('./config/mongoose')
 
 // setting template engine
@@ -20,6 +22,22 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.login_msg = req.flash('login_msg')
+  next()
+})
+
 app.use(routes)
 
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,23 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
-router.post('/login', (req, res) => {
-
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-
+  const errors = []
   User.findOne({ email }).then(user => {
-    if (user) {     
-
+    if (user) {
+      errors.push({ message: 'User already exists.' })
       return res.render('register', {
         errors,
         name,
