76c2b59941af79e37eb4977be1933201838ad43f
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-17 18:10:00 +0800

    feat: add user authentication in todo and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    req.flash('warning_msg', 'Please log in first.')
+    res.redirect('/users/login')
+  }
+}
+
+
