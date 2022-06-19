a14991cdc19961d5103d355a11810facd4d00c98
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-19 11:47:09 +0800

    fic: register name not required

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 const RestaurantSchema = new Schema({
   name: {
-    type: String
+    type: String,
+    required: true
   },
   name_en: {
     type: String,
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true
+    default: 'user'
   },
   email: {
     type: String,
